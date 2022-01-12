7ea96a322763a2a9f2d0e773b57905691d994807
Chris Meenen <meenenc@gmail.com>
2022-01-12 11:47:24 -0500

    first version with CRUD functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,4 @@ Rails.application.routes.draw do
   post "create", to: "leftpad#create"
   get "edit", to: "leftpad#edit"
   get "delete", to: "leftpad#delete"
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
